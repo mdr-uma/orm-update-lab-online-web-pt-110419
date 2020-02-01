2d6165c2523495e927892d8c96ba9821265320a9
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-01 03:08:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,4 +63,9 @@ class Student
     DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
   end
 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
 end
